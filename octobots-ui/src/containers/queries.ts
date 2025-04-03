855f51f73cd9651540a383a494b53f98953da71f
MKarzoun <mkarzoun@karzoun.com>
2025-04-03 15:37:52 +0300

    {commit_message}

--- octobots-ui/src/containers/queries.ts
+++
@@ -1,9 +0,0 @@
-const combinedFields = `
-  query fieldsCombinedByContentType($contentType: String!) {
-    fieldsCombinedByContentType(contentType: $contentType)
-  }
-`;
-
-export default {
-  combinedFields
-};
